f97a8f0dd5ef13f8aed035b7eedf6f49ee29144c
Zehan Khan <zehan9211@gmail.com>
2023-02-08 14:40:33 +0530

    add all

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './styles.css';
 
 const Footer = () => (
-  <div className="footer">
-    <p>© 2023 Zehan Khan</p>
-  </div>
+    <div className="footer">
+        <p>© 2023 Zehan Khan</p>
+    </div>
 )
 
 export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from './components/App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-      <Route exact path="/" component={<h1>Hello world</h1>} />
+    <App />
+      {/* <Routes>
+      <Route exact path="/" component={<App />} />
 
       <Route exact path="/NotFound" element={<h1>Not Found</h1>} />
-      </Routes>
+      </Routes> */}
     </BrowserRouter>
   </React.StrictMode>,
 )
